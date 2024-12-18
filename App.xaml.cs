d7a3e958395f26ddc9315bedb52c9f0fc1e9634b
Opilane TTHK <35493814+Overcrewt@users.noreply.github.com>
2024-12-18 10:54:39 +0200

    init commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KitchenCoPilot
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
