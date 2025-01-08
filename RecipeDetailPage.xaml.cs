b425fd7dbbde5ec749dea8402b7165981e703b30
Ken Koolmeister <kennu1512@gmail.com>
2025-01-08 12:39:05 +0200

    main branch without issues

--- RecipeDetailPage.xaml.cs
+++ RecipeDetailPage.xaml.cs
@@ -29,13 +29,7 @@ namespace KitchenCoPilot
             IngredientsCollectionView.ItemsSource = ingredients;
         }
 
-        private async void OnAddToGroceryListClicked(object sender, EventArgs e)
-        {
-            if (BindingContext is RecipeDetailViewModel viewModel && viewModel.SelectedRecipe != null)
-            {
-                await Navigation.PushAsync(new GroceryListPage(viewModel.SelectedRecipe));
-            }
-        }
+        
     }
 
     public class Ingredient
